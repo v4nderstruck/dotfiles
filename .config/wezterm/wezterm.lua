3ccbbfd7625ce72007c7c0d5adcfb0b3a072ea55
zensayyy <zensayyy@gmail.com>
2023-04-02 12:02:44 +0200

    wezterm: config bg and bindigs!

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -11,9 +11,31 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
+config.color_scheme = 'Darcula (base16)'
+config.font = wezterm.font("Hack Nerd Font")
+config.disable_default_key_bindings = true
 
--- For example, changing the color scheme:
-config.color_scheme = 'Calamity'
+-- config keybindings
+config.keys = {
+  {
+    key = "t",
+    mods = "CTRL",
+    action = wezterm.action.SpawnTab "CurrentPaneDomain"
+  },
+  {
+    key = "w",
+    mods = "CTRL",
+    action = wezterm.action.CloseCurrentTab{confirm=true} 
+  },
+}
+
+-- wezterm starts loading from HOME directory
+config.window_background_image = ".config/wezterm/bg-bladerunner49.jpg"
+config.window_background_image_hsb = {
+  brightness = 0.2,
+  hue = 1.0,
+  saturation = 1.0,
+}
 
 -- and finally, return the configuration to wezterm
 return config
